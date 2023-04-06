be791d32be271d54f599a2bcf9a4f4168af40818
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-04-06 23:10:42 +0900

    🚚 ディレクトリ移動

---
+++ client/src/boundaries/supabase/index.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  'https://icmgieijfinnhpwdyphu.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljbWdpZWlqZmlubmhwd2R5cGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA3ODQzODUsImV4cCI6MTk5NjM2MDM4NX0.hzPO1D02Fg2ktocCw6gdzXjb2Xj_C1wM74KfxeRIXns',
+);
