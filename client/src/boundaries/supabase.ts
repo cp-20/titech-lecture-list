bc11a9f277b06eed10e1e606760359ce1cc9438a
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-05-29 17:44:02 +0900

    🚧 Prismaの定義をいい感じにしてパフォーマンス向上

--- client/src/boundaries/supabase.ts
+++ client/src/boundaries/supabase.ts
@@ -32,6 +32,19 @@ export const searchLectures = async (
     ? { language: { in: searchQuery.language } }
     : {};
 
+  const placeQuery = {
+    periods: {
+      some: {
+        period: {
+          in: searchQuery.period,
+        },
+        day: {
+          in: searchQuery.day,
+        },
+      },
+    },
+  };
+
   try {
     const page = searchQuery.page ?? 1;
     const limit = searchQuery.limit ?? 50;
@@ -45,71 +58,41 @@ export const searchLectures = async (
             ...codeQuery,
             ...quarterQuery,
             ...languageQuery,
+            ...placeQuery,
           },
         ],
       },
+      include: { periods: true },
       skip: (page - 1) * limit,
       take: limit,
     });
 
-    const lecturePeriods = await prisma.lecturePeriod.findMany({
-      where: {
-        lecture_id: {
-          in: filteredLectures.map((lecture) => lecture.id),
-        },
+    const structuredLectures: lecture[] = filteredLectures.map((lecture) => ({
+      code: {
+        grade: lecture.code_grade as 100 | 200 | 300 | 400 | 500 | 600,
+        value: lecture.code_value,
       },
-    });
-
-    const enhancedFilteredLectures = filteredLectures.filter((lecture) => {
-      if (searchQuery.period === undefined && searchQuery.day === undefined) {
-        return true;
-      }
-
-      const periods = lecturePeriods.filter(
-        (period) => period.lecture_id === lecture.id,
-      );
-
-      return periods.some(
-        (period) =>
-          (searchQuery.period
-            ? searchQuery.period?.includes(period.period)
-            : true) &&
-          (searchQuery.day
-            ? (searchQuery.day as string[]).includes(period.day)
-            : true),
-      );
-    });
-
-    const structuredLectures: lecture[] = enhancedFilteredLectures.map(
-      (lecture) => ({
-        code: {
-          grade: lecture.code_grade as 100 | 200 | 300 | 400 | 500 | 600,
-          value: lecture.code_value,
-        },
-        title: {
-          ja: lecture.title_ja,
-          en: lecture.title_en ?? undefined,
-        },
-        teachers: lecture.teachers,
-        credit: lecture.credit,
-        language: lecture.language as '日本語' | '英語',
-        quarter: lecture.quarter as lectureQuarter,
-        year: lecture.year,
-        origin: lecture.origin,
-        link: lecture.link,
-        place: {
-          type: lecture.place_type,
-          value: lecture.place_value,
-          periods: lecturePeriods
-            .filter((period) => period.lecture_id === lecture.id)
-            .map((period) => ({
-              day: period.day,
-              period: period.period,
-              classroom: period.classroom,
-            })),
-        } as lecturePlace,
-      }),
-    );
+      title: {
+        ja: lecture.title_ja,
+        en: lecture.title_en ?? undefined,
+      },
+      teachers: lecture.teachers,
+      credit: lecture.credit,
+      language: lecture.language as '日本語' | '英語',
+      quarter: lecture.quarter as lectureQuarter,
+      year: lecture.year,
+      origin: lecture.origin,
+      link: lecture.link,
+      place: {
+        type: lecture.place_type,
+        value: lecture.place_value,
+        periods: lecture.periods.map((period) => ({
+          day: period.day,
+          period: period.period,
+          classroom: period.classroom,
+        })),
+      } as lecturePlace,
+    }));
 
     return {
       finish: structuredLectures.length === 0,
